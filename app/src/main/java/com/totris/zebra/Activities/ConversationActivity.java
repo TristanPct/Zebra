3b1539bab4ee7d9b03b1c3c7d764a0b852c235ac
Thomas Lecoeur <me@thomaslecoeur.com>
2016-10-11 15:29:26 +0200

    Add basic conversation activity

---
+++ app/src/main/java/com/totris/zebra/Activities/ConversationActivity.java
@@ -0,0 +1,15 @@
+package com.totris.zebra.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.totris.zebra.R;
+
+public class ConversationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+    }
+}
