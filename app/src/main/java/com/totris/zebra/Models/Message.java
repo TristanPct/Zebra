cf9b6566881fb6c8a1eedbbb5e94147e0796dcca
Tristan Picut <tristan@picut.fr>
2016-10-11 15:34:47 +0200

    Create Message model

---
+++ app/src/main/java/com/totris/zebra/Models/Message.java
@@ -0,0 +1,73 @@
+package com.totris.zebra.Models;
+
+import java.util.Date;
+
+public class Message {
+    private String content;
+    private int type;
+    private int senderId;
+    private int groupId;
+    private Date createdAt;
+    private Date sentAt;
+    private Date receiveAt;
+
+    public Message() {
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(Date sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public Date getReceiveAt() {
+        return receiveAt;
+    }
+
+    public void setReceiveAt(Date receiveAt) {
+        this.receiveAt = receiveAt;
+    }
+}
