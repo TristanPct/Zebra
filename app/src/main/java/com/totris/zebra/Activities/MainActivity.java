65df6da217dc26a9f1b2712ca002d63c61544dc4
Tristan Picut <tristan@picut.fr>
2016-10-11 15:21:18 +0200

    Change folders structure

--- app/src/main/java/com/totris/zebra/Activities/MainActivity.java
+++ app/src/main/java/com/totris/zebra/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.totris.zebra;
+package com.totris.zebra.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.totris.zebra.R;
 
 public class MainActivity extends AppCompatActivity {
 
