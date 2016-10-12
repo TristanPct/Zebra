0ecd70342ac766ac1fbc6800c2c72569f1880766
Tristan Picut <tristan@picut.fr>
2016-10-12 16:37:27 +0200

    Fix and clear imports

--- app/src/main/java/com/totris/zebra/Activities/ContactActivity.java
+++ app/src/main/java/com/totris/zebra/Activities/ContactActivity.java
@@ -4,27 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-import com.futuremind.recyclerviewfastscroll.FastScroller;
-import com.totris.zebra.ContactListFragment;
-import com.totris.zebra.ContactsAdapter;
-import com.totris.zebra.Models.Message;
-import com.totris.zebra.Models.User;
+import com.totris.zebra.Fragments.ContactListFragment;
+import com.totris.zebra.Fragments.ContactsAdapter;
 import com.totris.zebra.R;
-
-import org.jdeferred.DoneCallback;
-import org.jdeferred.Promise;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class ContactActivity extends AppCompatActivity implements ContactsAdapter.ContactItemListener {
     private static String TAG = "ContactActivity";
