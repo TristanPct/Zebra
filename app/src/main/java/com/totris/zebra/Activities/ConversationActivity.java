5bcc5d9c88b352c287a7b4f6a4fd3224eab9add1
Tristan Picut <tristan@picut.fr>
2016-10-12 17:10:31 +0200

    Add conversation fragment

--- app/src/main/java/com/totris/zebra/Activities/ConversationActivity.java
+++ app/src/main/java/com/totris/zebra/Activities/ConversationActivity.java
@@ -1,117 +1,51 @@
 package com.totris.zebra.Activities;
 
-import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-import com.squareup.otto.Subscribe;
-import com.totris.zebra.Events.MessageDataChangeEvent;
-import com.totris.zebra.Fragments.MessagesAdapter;
-import com.totris.zebra.Models.EncryptedMessage;
+import com.totris.zebra.Fragments.ConversationFragment;
 import com.totris.zebra.Models.Message;
 import com.totris.zebra.Models.MessageType;
 import com.totris.zebra.R;
-import com.totris.zebra.Utils.Database;
 import com.totris.zebra.Utils.EventBus;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-public class ConversationActivity extends AppCompatActivity {
+public class ConversationActivity extends AppCompatActivity implements ConversationFragment.ConversationListener {
     static String TAG = "ConversationActivity";
 
-    private MessagesAdapter adapter;
-
-    @BindView(R.id.messagesList) //TODO: bind all those stuff in a fragment
-    RecyclerView messagesListRecyclerView;
-
-    @BindView(R.id.messageInput)
-    TextView messageInput;
-
-    @BindView(R.id.messageSubmit)
-    Button messageSubmit;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
-        ButterKnife.bind(this);
+        currentFragment = new ConversationFragment();
 
-        messagesListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        List<Message> messages = new ArrayList<>();
-
-        messages.add(new Message("Loading messages...", MessageType.TEXT, 0, 0));
-
-        adapter = new MessagesAdapter(messages);
-
-        adapter.setOnMessageItemListener(new MessagesAdapter.MessageItemListener() {
-            @Override
-            public void onMessageItemClick(int position, View v) {
-                Log.d(TAG, "onMessageItemClick: " + position);
-            }
-        });
-
-        messagesListRecyclerView.setAdapter(adapter);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.activity_conversation, currentFragment)
+                    .commit();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        EventBus.register(this);
+        EventBus.register(currentFragment);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        EventBus.unregister(this);
+        EventBus.unregister(currentFragment);
     }
 
-    @Subscribe
-    public void onMessageDataChangeEvent(MessageDataChangeEvent event) {
-        Log.d(TAG, "onDataChange: new message");
-        List<Message> messages = event.getMessages();
-
-        adapter.clear();
-
-        for (Message message : messages) {
-            adapter.addMessage(message); // TODO: only add not already fetched messages
-        }
-
-        adapter.notifyDataSetChanged();
-    }
-
-    @OnClick(R.id.messageSubmit)
-    public void submitMessage(Button button) {
-        Message message = new Message(messageInput.getText().toString(), MessageType.TEXT, 0, 0);
-        message.send();
-
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-
-        messageInput.setText("");
-
-        /*LinearLayoutManager recyclerViewLayoutManager = (LinearLayoutManager) contactsListRecyclerView.getLayoutManager();
-        recyclerViewLayoutManager.setStackFromEnd(true);*/
-
+    @Override
+    public void onSubmitMessage(String message) {
+        Log.d(TAG, "onSubmitMessage: " + message);
+        new Message(message, MessageType.TEXT, 0, 0).send();
     }
 }
---
+++ app/src/main/java/com/totris/zebra/Fragments/ConversationFragment.java
@@ -0,0 +1,127 @@
+package com.totris.zebra.Fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.squareup.otto.Subscribe;
+import com.totris.zebra.Events.MessageDataChangeEvent;
+import com.totris.zebra.Fragments.MessagesAdapter;
+import com.totris.zebra.Models.Message;
+import com.totris.zebra.Models.MessageType;
+import com.totris.zebra.R;
+import com.totris.zebra.Utils.EventBus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConversationFragment extends Fragment {
+    private static final String TAG = "ConversationFragment";
+
+    private ConversationListener listener;
+
+    private MessagesAdapter adapter;
+
+    @BindView(R.id.messagesList) //TODO: bind all those stuff in a fragment
+    RecyclerView messagesListRecyclerView;
+
+    @BindView(R.id.messageInput)
+    TextView messageInput;
+
+    @BindView(R.id.messageSubmit)
+    Button messageSubmit;
+
+    public ConversationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (ConversationListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement ConversationListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_conversation, container, false);
+
+        ButterKnife.bind(this, view);
+
+        messagesListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        List<Message> messages = new ArrayList<>();
+
+        messages.add(new Message("Loading messages...", MessageType.TEXT, 0, 0));
+
+        adapter = new MessagesAdapter(messages);
+
+        adapter.setOnMessageItemListener(new MessagesAdapter.MessageItemListener() {
+            @Override
+            public void onMessageItemClick(int position, View v) {
+                Log.d(TAG, "onMessageItemClick: " + position);
+            }
+        });
+
+        messagesListRecyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Subscribe
+    public void onMessageDataChangeEvent(MessageDataChangeEvent event) {
+        Log.d(TAG, "onDataChange: new message");
+        List<Message> messages = event.getMessages();
+
+        adapter.clear();
+
+        for (Message message : messages) {
+            adapter.addMessage(message); // TODO: only add not already fetched messages
+        }
+
+        adapter.notifyDataSetChanged();
+    }
+
+    @OnClick(R.id.messageSubmit)
+    public void onSubmitMessage(Button button) {
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        messageInput.setText("");
+
+        if (listener != null) {
+            listener.onSubmitMessage(messageInput.getText().toString());
+        }
+    }
+
+    public interface ConversationListener {
+        void onSubmitMessage(String message);
+    }
+
+}
