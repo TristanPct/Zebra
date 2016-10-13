bb35a862212d26a1c87a0acfa2274d0c1ad6b49c
Tristan Picut <tristan@picut.fr>
2016-10-13 20:19:08 +0200

    Rename ContactActivity to ContactListActivity

--- app/src/main/java/com/totris/zebra/Activities/ContactListActivity.java
+++ app/src/main/java/com/totris/zebra/Activities/ContactListActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -24,7 +23,7 @@ public class ContactListActivity extends AppCompatActivity implements ContactsAd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        setContentView(R.layout.activity_contact_list);
 
         if(savedInstanceState == null) {
             ContactListFragment contactsList = new ContactListFragment();
